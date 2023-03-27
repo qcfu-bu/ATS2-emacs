3138a7c83792668220486bfdfc41f6d4d5921ab7
qcfu <qcfu@bu.edu>
2023-03-27 00:57:24 -0400

    removed flymake

--- ats2-mode.el
+++ ats2-mode.el
@@ -377,51 +377,6 @@
                       (back-to-indentation)
                       (current-column))))
 
-(defvar ats2-flymake-command
-  "patscc"
-  "Command used to check an ATS2 file for errors")
-
-(defvar ats2-flymake-command-options
-  "-tcats"
-  "Options passed to the command used to check a file for errors")
-
-(defun ats2-flymake-init ()
-  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-	 (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list ats2-flymake-command
-          (list ats2-flymake-command-options local-file))))
-
-;; List of file extensions that trigger ats2-flymake.
-(push '(".+\\.sats$" ats2-flymake-init flymake-simple-cleanup) flymake-allowed-file-name-masks)
-(push '(".+\\.dats$" ats2-flymake-init flymake-simple-cleanup) flymake-allowed-file-name-masks)
-(push '(".+\\.hats$" ats2-flymake-init flymake-simple-cleanup) flymake-allowed-file-name-masks)
-
-;; Regular expressions for detecting and reporting errors.
-(push '("^\\(syntax error\\): *\\([^ ]+\\):.*line=\\([0-9]+\\).*$" 2 3 nil 1)
-      flymake-err-line-patterns)
-(push '("^\\(.+.dats\\|.sats\\|.hats\\):.*line=\\([0-9]+\\).*\\(error.+\\)$" 1 2 nil 3)
-      flymake-err-line-patterns)
-
-(defun ats2-flymake-load ()
-  (flymake-mode t)
-
-  ;; Utility key bindings for navigating errors reported by flymake.
-  (local-set-key (kbd "C-c C-d") 'flymake-display-err-menu-for-current-line)
-  (local-set-key (kbd "C-c C-n") 'flymake-goto-next-error)
-  (local-set-key (kbd "C-c C-p") 'flymake-goto-prev-error)
-
-  ;; Prevents flymake from throwing a configuration error
-  ;; This must be done because atsopt returns a non-zero return value
-  ;; when it finds an error, flymake expects a zero return value.
-  (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-    (setq flymake-check-was-interrupted t))
-  (ad-activate 'flymake-post-syntax-check))
-
-(add-hook 'ats2-mode-hook 'ats2-flymake-load)
-
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.[dsh]ats\\'" . ats2-mode))
 
